219d036f66cd6e057de81a31035b8b940a9118a4
dipeshkhatiwada <deepaacee@gmail.com>
2020-07-11 22:33:47 +0545

    edit delete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,8 +12,11 @@
             <div class="c0l-12 col-lg-6">
                 <ul class="list-group">
                     <Todo v-for="(todo, index) in todos" :key="index"
-                    :todoString="todo.todoString"
-                    :completed="todo.completed"
+                        :todoString="todo.todoString"
+                        :completed="todo.completed"
+                        @on-delete="deleteTodo(todo)"
+                        @on-toggle="toggleTodo(todo)"
+                        @on-edit="editTodo(todo, $event)"
                     />
                 </ul>
             </div>
@@ -38,23 +41,24 @@ export default {
           ]
         }
     },
-    methods:{
-        addTodo(newTodo){
-            this.todos.push({
-                todoString: newTodo,
-                completed:false
-            });
+    methods: {
+        addTodo(newTodo) {
+        this.todos.push({
+            todoString: newTodo,
+            completed: false
+        });
         },
-        toggleTodo(todo){
-            todo.completed = !todo.completed;
+        toggleTodo(todo) {
+        todo.completed = !todo.completed;
         },
-        editTodo(todo, newTodoString){
-            todo.todoString = newTodoString;
+        editTodo(todo, newTodoString) {
+        todo.todoString = newTodoString;
         },
-        deleteTodo(deleteTodo){
-            this.todos = this.todos.fiter(todo => todo.todoString !== deleteTodo)
-        },
+        deleteTodo(deleteTodo) {
+        this.todos = this.todos.filter(todo => todo !== deleteTodo);
         }
+    }
+
 }
 </script>
 <style>
